679922ba2737e3b10a74d4e6f551d6a353c583cd
Andrew Cerveny <andrewfcerveny0628@gmail.com>
2022-11-28 19:43:19 -0500

    Add completed Cards class based on prompt provided

---
+++ src/Card.js
@@ -0,0 +1,11 @@
+class Card {
+    constructor(num,question,selections,correctAnswer) {
+        this.id = num;
+        this.question = question;
+        this.answers = selections;
+        this.correctAnswer = correctAnswer
+
+    }
+}
+
+module.exports =  Card;
